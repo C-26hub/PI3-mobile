40a12efb2764f86f2c2f59dc61f61de3ed85ee3d
Lipe07-xds <xfilipe2006.santos@gmail.com>
2026-06-02 12:04:10 -0300

    feat(login): adiciona validação e desabilita botão quando campos estão vazios

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from 'react-native';
 import { Link, router } from "expo-router";
+import { useState } from 'react';
 
 export default function Home() {
+
+  const [login, setLogin] = useState('');
+  const [senha, setSenha] = useState('');
+
   return (
     <View style={styles.container}>
       <View style={styles.card}>
@@ -16,7 +21,26 @@ export default function Home() {
           Informe seus dados para prosseguir
         </Text>
 
-        <TouchableOpacity style={styles.botao} onPress={() => router.push("/dashboard")}>
+        <TextInput
+          style={[styles.input, styles.marginTop]}
+          placeholder="Login"
+          placeholderTextColor="#999"
+          value={login}
+          onChangeText={setLogin}
+        />
+
+        <TextInput
+          style={[styles.input, styles.marginBottom]}
+          placeholder="Senha"
+          placeholderTextColor="#999"
+          secureTextEntry
+          value={senha}
+          onChangeText={setSenha}
+        />
+
+        <TouchableOpacity style={[styles.botao, (!login || !senha) && { opacity: 0.5 }]}
+        disabled={!login || !senha}
+        onPress={() => router.push("/dashboard")}>
           <Text style={styles.textoBotao}>Entrar</Text>
         </TouchableOpacity>
 
@@ -60,6 +84,25 @@ const styles = StyleSheet.create({
     color: '#004A8D',
     marginVertical: 20,
     textAlign: 'center',
+  },
+
+  input: {
+    width: '100%',
+    height: 42,
+    backgroundColor: '#F5F5F5',
+    borderWidth: 1,
+    borderColor: '#999',
+    borderRadius: 6,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+
+  marginTop: {
+    marginTop: 10,
+  },
+
+  marginBottom: {
+    marginBottom: 10,
   },
 
   botao: {
